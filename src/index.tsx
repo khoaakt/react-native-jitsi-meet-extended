d21f175e486d903797afc4ccdf1a8b6401c3195f
Nguyễn Anh Khoa <khoaakt@gmail.com>
2021-12-29 03:47:37 +0700

    fix: ios platform specific code

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,26 @@
-import { NativeModules, requireNativeComponent } from 'react-native';
+import { NativeModules, requireNativeComponent, Platform } from 'react-native';
 
-const { JitsiMeetExtended, JitsiMeetExtendedView } = NativeModules;
+const { JitsiMeetExtended } = NativeModules;
 const JitsiMeetView = requireNativeComponent("JitsiView");
 
-const call = JitsiMeetExtendedView.call;
-const audioCall = JitsiMeetExtendedView.audioCall;
-JitsiMeetExtendedView.call = (url: any, userInfo: any) => {
-  userInfo = userInfo || {};
-  call(url, userInfo);
-}
-JitsiMeetExtendedView.audioCall = (url: any, userInfo: any) => {
-  userInfo = userInfo || {};
-  audioCall(url, userInfo);
+const isIOS = Platform.OS === 'ios';
+
+let JitsiMeetViewIOS = null; 
+let JitsiMeetModuleIOS = null; 
+
+if (isIOS) {
+  JitsiMeetViewIOS = requireNativeComponent('JitsiMeetExtendedView');;
+  JitsiMeetModuleIOS = NativeModules.JitsiMeetExtendedView;
+  const call = JitsiMeetModuleIOS.call;
+  const audioCall = JitsiMeetModuleIOS.audioCall;
+  JitsiMeetModuleIOS.call = (url: any, userInfo: any) => {
+    userInfo = userInfo || {};
+    call(url, userInfo);
+  }
+  JitsiMeetModuleIOS.audioCall = (url: any, userInfo: any) => {
+    userInfo = userInfo || {};
+    audioCall(url, userInfo);
+  }
 }
 
-export { JitsiMeetExtended, JitsiMeetView, JitsiMeetExtendedView };
+export { JitsiMeetExtended, JitsiMeetView, JitsiMeetModuleIOS, JitsiMeetViewIOS };
